e910e404eaf1d942f318478839cc3e9a78bb8436
kxhuan <kxhuan@dolby.com>
2017-09-29 09:20:33 +1000

    improve bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,10 @@
-git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+#!/bin/bash -x
+HOME=$(echo ~)
+VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
+echo $VUNDLE_DIR
+if [ ! -d $VUNDLE_DIR ]; then
+    git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+fi
+cp .vimrc ~
+cp .tmux.conf ~
 vim +PluginInstall

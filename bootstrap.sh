9d9dc18b5ca17e91cad691719c34d484383ec3d8
Kun-Yao <canyonh@gmail.com>
2018-02-21 11:39:13 +1100

    make it work for linux mint

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,10 +16,15 @@ if [ ! -d $VUNDLE_DIR ]; then
     git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 fi
 
+if [[ "$platform" == 'linux' ]]; then
+	sudo apt-get install build-essential cmake
+	sudo apt-get install python-dev python3-dev
+fi
+
 # install plugin silently
-echo | echo | vim +PluginInstall +qall &>/dev/null
-#vim +PluginInstall
-~/.vim/bundle/YouCompleteMe/install.py --clang-complete
+#echo | echo | vim +PluginInstall +qall &>/dev/null
+vim +PluginInstall
+$HOME/.vim/bundle/YouCompleteMe/install.py --clang-complete
 
 #zsh
 if [[ "$platform" == 'linux' ]]; then
@@ -29,6 +34,9 @@ else
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo "source $HOME/devenv/vim/.vimrc" > ~/.vimrc 
+echo "source $HOME/devenv/zsh/.zshrc" > ~/.zshrc
+echo "source $HOME/devenv/tmux/.tmux.conf" > ~/.tmux.conf

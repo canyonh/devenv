8c0c8df6fc8e600e3c74b43186cabb08e5277529
kxhuan <canyonh@gmail.com>
2019-03-19 14:19:33 +1100

    add line to install zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,9 +37,9 @@ if [[ "$platform" == 'linux' ]]; then
 else
 	brew install zsh
 fi
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
